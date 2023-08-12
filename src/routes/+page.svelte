b49696722b789c3d64e6ac64efef06f0ab5ad76c
RyuseiNomi <kanontonto1010@gmail.com>
2023-08-12 12:53:40 +0900

    add logic to add members

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  let members = [];
+
+  onMount(async () => {
+    const button = document.querySelector("button");
+    const input = document.querySelector("input");
+
+    button.addEventListener("click", (event) => {
+      if ( input.value === '') { return; }
+      members.push(input.value);
+      members = members;
+      console.log(members);
+    })
+  })
+</script>
+
+<div class="page__members">
+  {#each members as member}
+    <p>{member}</p>
+  {/each}
+</div>
+<input class="page__name-input">
+<button class="page__name-add">＋</button>
+
 <nav>
   <a href="/result">チョイスする</a>
 </nav>
