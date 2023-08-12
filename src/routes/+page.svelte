b1f1b0e1c187dd5f005e7511b47b4cb97cba9dd7
RyuseiNomi <kanontonto1010@gmail.com>
2023-08-12 13:17:29 +0900

    add dropdown to select group number

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   let members = [];
+  let maxGroupNumber = 0;
 
   onMount(async () => {
     const button = document.querySelector("button");
@@ -9,19 +10,29 @@
     button.addEventListener("click", (event) => {
       if ( input.value === '') { return; }
       members.push(input.value);
+      maxGroupNumber = members.length;
       members = members;
-      console.log(members);
     })
   })
 </script>
 
-<div class="page__members">
+<div class="top__members">
   {#each members as member}
     <p>{member}</p>
   {/each}
 </div>
-<input class="page__name-input">
-<button class="page__name-add">＋</button>
+
+<div class="top__input-area">
+  <input class="top__name-input">
+  <button class="top__name-add">＋</button>
+</div>
+
+<select>
+  <option value="">--組み分け数を選択--</option>
+  {#each {length: maxGroupNumber} as _, i}
+    <option value={i+1}>{i+1}</option>
+  {/each}
+</select>
 
 <nav>
   <a href="/result">チョイスする</a>
