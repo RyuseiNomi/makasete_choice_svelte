becf1ec65eea1f6f8c7b7f37f1c40a70448b455b
RyuseiNomi <kanontonto1010@gmail.com>
2023-08-12 19:20:06 +0900

    adjust style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  let name = '';
   let members = [];
   let groupNumber = 0;
   let maxGroupNumber = 0;
@@ -18,46 +19,117 @@
     shuffledMembers = responseJson.assignedMembers;
   }
 
-  onMount(async () => {
-    const nameAddButton = document.querySelector(".top__name-add");
-    const input = document.querySelector("input");
-
-    nameAddButton.addEventListener("click", (event) => {
-      if ( input.value === '') { return; }
-      members.push(input.value);
-      maxGroupNumber = members.length;
-      members = members;
-    });
-  })
+  function addMember(value) {
+    if ( value === '') { return; }
+    members.push(value);
+    maxGroupNumber = members.length;
+    members = members;
+  }
 </script>
 
-<div class="top__members">
-  {#each members as member}
-    <p>{member}</p>
-  {/each}
-</div>
-
-<div class="top__input-area">
-  <input class="top__name-input">
-  <button class="top__name-add">＋</button>
-</div>
-
-<select bind:value={groupNumber}>
-  <option value="">--組み分け数を選択--</option>
-  {#each {length: maxGroupNumber} as _, i}
-    <option value={i+1}>{i+1}</option>
-  {/each}
-</select>
-
-<div class="top__submit-area" on:click={choice}>
-  <button>チョイスする</button>
-</div>
-
-<div class="top__result">
-  {#each shuffledMembers as group, i}
-    <p>Group {i+1}</p>
-    {#each group as _, i}
-      <p>{group[i]}</p>
+<div class="top__body">
+  <div class="top__members">
+    {#each members as member}
+      <p>{member}</p>
     {/each}
-  {/each}
+  </div>
+  
+  <div class="top__input-area">
+    <p>1. メンバーの名前を入力</p>
+    <input
+      placeholder="メンバー名を入力"
+      bind:value={name}
+      on:keydown={async (e) => {
+        if ( e.key == 'Enter' ) {
+          addMember(name);
+          name = '';
+        }
+      }}
+    >
+  </div>
+  
+  <div class="top__group-input">
+    <p>2. グループの数を入力</p>
+    <select bind:value={groupNumber}>
+      <option value="">--組み分け数を選択--</option>
+      {#each {length: maxGroupNumber} as _, i}
+        <option value={i+1}>{i+1}</option>
+      {/each}
+    </select>
+  </div>
+  
+  <div class="top__submit-area" on:click={choice}>
+    <button>チョイスする</button>
+  </div>
+  
+  <div class="top__result">
+    {#each shuffledMembers as group, i}
+      <p class="top__result-group">Group {i+1}</p>
+      {#each group as _, i}
+        {#if group[i] !== null}
+          <p>{group[i]}</p>
+        {/if}
+      {/each}
+    {/each}
+  </div>
 </div>
+
+<style lang="scss" scoped>
+  .top__body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow-y: scroll;
+
+    p {
+      font-family: "ヒラギノ角ゴ ProN W3";
+      color: #696969;
+    }
+  }
+
+  .top__members {
+    width: 100%;
+    height: 40vh;
+    overflow-y: scroll;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .top__input-area {
+    width: 80%;
+    padding-bottom: 30px;
+
+    input {
+      width: 100%;
+      height: 30px;
+    }
+  }
+
+  .top__group-input {
+    width: 80%;
+    select {
+      width: 100%;
+      margin-bottom: 30px;
+      height: 20px;
+    }
+  }
+
+  .top__submit-area {
+      button {
+        background: #E6A0BD;
+        border: none;
+        color: white;
+        font-size: 30px;
+        border-radius: 6px;
+      }
+  }
+
+  .top__result {
+    width: 80%;
+  }
+  .top__result-group {
+    font-size: 24px;
+  }
+</style>
